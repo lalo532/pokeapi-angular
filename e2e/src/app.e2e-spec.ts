8be564e8be5fba74de994bd31d0be3ce5892b7a7
xAlexei <61245046+xAlexei@users.noreply.github.com>
2022-03-23 15:07:38 -0600

    [FINISHED]

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pokedex app is running!');
+    expect(page.getTitleText()).toEqual('Pokeapi is running!');
   });
 
   afterEach(async () => {
